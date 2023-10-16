419ed0dd3543a3cf4ce8f3b8c070a467b9663271
Arthur Pereira da Silva <72280602+ArthurPdaSilva@users.noreply.github.com>
2023-10-16 19:53:56 -0300

    feat: Projeto da Calculadora básica

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Calculadora
+    {
+        public float NumberOne { get; set; }
+        public float NumberTwo { get; set; }
+
+        public Calculadora(float n1, float n2)
+        {
+            NumberOne = n1;
+            NumberTwo = n2;
+        }
+
+        public float Somar()
+        {
+            return NumberOne + NumberTwo;
+        }
+
+        public float Diminuir()
+        {
+            return NumberOne - NumberTwo;
+        }
+
+        public float Multiplicar()
+        {
+            return NumberOne * NumberTwo;
+        }
+
+        public float Dividir()
+        {
+            return NumberOne / NumberTwo;
+        }
+    }
+}
--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Digite o primeiro e o segundo número separando por espaço:");
+            string[] inputs = Console.ReadLine().Split(" ");
+
+            if (inputs.Length >= 2)
+            {
+                float firstNumber = int.Parse(inputs[0]);
+                float secondNumber = int.Parse(inputs[1]);
+                Calculadora calculadora = new Calculadora(firstNumber, secondNumber);
+
+                Console.WriteLine("""
+                Selecione a opção desejada:
+                0-Somar
+                1-Diminuir
+                2-Multiplicar
+                3-Dividir
+            """);
+
+                int options = int.Parse(Console.ReadLine());
+                bool isValid = true;
+                string opt = "+";
+                float result = calculadora.Somar();
+                switch (options)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        result = calculadora.Diminuir();
+                        opt = "-";
+                        break;
+                    case 2:
+                        result = calculadora.Multiplicar();
+                        opt = "*";
+                        break;
+                    case 3:
+                        result = calculadora.Dividir();
+                        opt = "/";
+                        break;
+                    default:
+                        isValid = false;
+                        break;
+                }
+
+                if (isValid) Console.WriteLine($"{firstNumber} {opt} {secondNumber} = {result} ");
+                else Console.WriteLine("Opção inválida!");
+            }
+            else Console.WriteLine("Entrada inválida. Certifique-se de digitar dois números separados por espaço.");
         }
     }
 }
