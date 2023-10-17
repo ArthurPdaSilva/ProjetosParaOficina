67ab94c98a7d14be3263520c32578cb5db382f18
Arthur Pereira da Silva <72280602+ArthurPdaSilva@users.noreply.github.com>
2023-10-16 21:17:48 -0300

    feat: adição do todolist

---
+++ Todo/ListaDeTarefas.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo
+{
+    internal class ListaDeTarefas
+    {
+        private List<string> _itens;
+
+        public ListaDeTarefas()
+        {
+            _itens = new List<string>();
+        }
+
+        public bool AdicionarTarefa(string tarefa)
+        {
+            if (IsTarefaEncontrada(tarefa)) return false;
+
+            _itens.Add(tarefa);
+            return true;
+        }
+
+        public bool RemoverTarefa(string tarefa)
+        {
+            if (!IsTarefaEncontrada(tarefa))return false;
+
+            _itens.Remove(tarefa);
+            return true;
+        }
+
+        private bool IsTarefaEncontrada(string tarefa)
+        {
+            return _itens.Contains(tarefa);
+        }
+
+        public void ListarTarefas()
+        {
+            if (_itens.Count == 0) Console.WriteLine("Lista vazia.");
+            else
+            {
+                Console.WriteLine("Lista de Tarefas:");
+                foreach (var t in _itens)
+                {
+                    Console.WriteLine(t);
+                }
+            }
+
+        }
+
+
+    }
+}
---
+++ Todo/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Todo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string isOver = "4";
+            string task;
+            string message = "";
+            string menu = @"
+                0-Adicionar Tarefa
+                1-Remover uma tarefa
+                2-Listar Tarefas
+                3-Mostrar menu novamente
+                4-Fechar Programa
+            ";
+
+            ListaDeTarefas listaDeTarefas = new ListaDeTarefas();
+
+            Console.WriteLine("Selecione a opção desejada:");
+            Console.WriteLine(menu);
+            string opt = Console.ReadLine();
+
+            while (opt != isOver)
+            {
+                switch (opt)
+                {
+                    case "0":
+                        Console.WriteLine("Qual tarefa será adicionada?");
+                        task = Console.ReadLine();
+                        if (listaDeTarefas.AdicionarTarefa(task)) message = "Item adicionado com sucesso";
+                        else message = "Item já encontrado na lista";
+                        break;
+                    case "1":
+                        Console.WriteLine("Qual tarefa será removida?");
+                        task = Console.ReadLine();
+                        if (listaDeTarefas.RemoverTarefa(task)) message = "Item removido com sucesso";
+                        else message = "Item não encontrado na lista";
+                        break;
+                    case "2":
+                        listaDeTarefas.ListarTarefas();
+                        break;
+                    case "3":
+                        Console.WriteLine(menu);
+                        break;
+                    case "4":
+                        Console.WriteLine("Saindo do programa.");
+
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida, selecione opção coerente a lista");
+                        Console.WriteLine(menu);
+                        break;
+
+                }
+
+                if (opt != isOver)
+                {
+                    if(opt == "0" || opt == "1") Console.WriteLine(message);
+                    Console.WriteLine();
+                    Console.WriteLine("E agora, deseja fazer o quê?");
+                    opt = Console.ReadLine();
+                }
+
+
+            }
+
+        }
+    }
+}
